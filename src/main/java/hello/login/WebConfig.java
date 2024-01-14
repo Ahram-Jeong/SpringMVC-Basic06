bd96f559abe8ee29fdfd39d6ebb31b2ea51c9d10
Ahram-Jeong <ahram1215@gmail.com>
2024-01-15 00:35:57 +0900

    20240115 - 서블릿 필터_인증 체크

--- src/main/java/hello/login/WebConfig.java
+++ src/main/java/hello/login/WebConfig.java
@@ -1,6 +1,7 @@
 package hello.login;
 
 import hello.login.web.filter.LogFilter;
+import hello.login.web.filter.LoginCheckFilter;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,6 +16,16 @@ public class WebConfig {
         FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
         filterRegistrationBean.setFilter(new LogFilter()); // 적용할 필터
         filterRegistrationBean.setOrder(1); // 필터의 순서
+        filterRegistrationBean.addUrlPatterns("/*"); // 필터를 적용할 url패턴
+
+        return filterRegistrationBean;
+    }
+
+    @Bean
+    public FilterRegistrationBean logCheckFilter() {
+        FilterRegistrationBean<Filter> filterRegistrationBean = new FilterRegistrationBean<>();
+        filterRegistrationBean.setFilter(new LoginCheckFilter()); // 적용할 필터
+        filterRegistrationBean.setOrder(2); // 필터의 순서
         filterRegistrationBean.addUrlPatterns("/*"); // 필터를 적용할 url패턴
 
         return filterRegistrationBean;
